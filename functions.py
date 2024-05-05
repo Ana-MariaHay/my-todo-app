827abce411871d0449cfcbb8853fd1a5145bbaa3
ana maria hay <anahay66@hotmail.com>
2024-05-05 22:26:02 +0200

    create basic GUI without interactions, day 16

--- functions.py
+++ functions.py
@@ -2,26 +2,18 @@ FILEPATH = "todos.txt"
 
 
 def get_todos(filepath=FILEPATH):
-    """ open the file name todos.txt and return
-    the list of todo items
+    """ open a text file and return the list of
+    to-do items.
     """
     with open(filepath, 'r') as file:
-        todos_rec = file.readlines()
-
-    # remove '/n' and null at end of list
-    todos_local = [i.strip('\n') for i in todos_rec]
+        todos_local = file.readlines()
     return todos_local
 
 
-def write_todos(todos_local, filepath=FILEPATH):
-    """ write the file name todos.txt and update
-    the list of todo items
-    """
-    # add '/n' to item before write of file
-    todos_rec = [f"{i.title()}\n" for i in todos_local]
-
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
-        file.writelines(todos_rec)
+        file.writelines(todos_arg)
     return
 
 
